3f0b5a107d3ccb70aa8e325f3da1a95f000f0b43
Tiendaluque <119287138+Tiendaluque@users.noreply.github.com>
2022-11-28 10:27:28 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,48 @@
+jQuery('document').ready(function($) {
+    var menuBtn=$('.menu-icon'),
+    menu=$('.navigation ul');
+
+    menuBtn.click(function(){
+
+
+        if(menu.hasClass('show')){
+            menu.removeClass('show');
+        }
+        else{
+            menu.addClas('show');
+        }
+    });
+
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
